8d3282ac6d8e9bef302a6e8ae012cd41f734dcfa
Hugo Lafleur <gohu.ruelfal88@gmail.com>
2025-11-20 19:08:16 -0500

    small fix for the drag preview

--- src/BaseComponents/Card.tsx
+++ src/BaseComponents/Card.tsx
@@ -3,30 +3,28 @@ import { cardSuites } from "../Types/cardSuites.tsx";
 import { componentType } from "../Types/cardType.tsx";
 import { useDrag } from "react-dnd";
 import type {CardInfos} from "../Types/cardInfos.tsx";
-import {useEffect, useRef} from "react";
 
 function Card(card : CardInfos) {
-    const cardRef = useRef<HTMLDivElement>(null);
     const [{isDragging}, drag, preview] = useDrag(() => ({
+        item: card,
         type: componentType.CARD,
         collect: (monitor) => ({
             isDragging: monitor.isDragging()
         })
     }), [card])
 
-    useEffect(() => {
-        preview(cardRef.current, { captureDraggingState: true });
-    }, [preview]);
-
-    drag(cardRef)
-
     const cardColor = (card.suite === cardSuites.HEARTS || card.suite === cardSuites.DIAMONDS) ? "red" : "black"
     const cardToShow = getCardFromValues(card)
-    return <div ref={cardRef} style={{
-        color : cardColor,
-        fontSize : "8rem",
-        opacity: isDragging? 0.5 : 1,
-        cursor: "grab",
+    return <div
+        ref={node => {
+            drag(node)
+            preview(node)
+        }}
+        style={{
+            color : cardColor,
+            fontSize : "8rem",
+            opacity: isDragging? 0.5 : 1,
+            cursor: "grab",
     }}>{cardToShow}</div>
 }
 
